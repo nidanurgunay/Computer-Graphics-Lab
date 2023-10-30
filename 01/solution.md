e04aa2455509f6e60e47a941224f966b118c3934
Patrick Paetzold <patrick.paetzold@uni-konstanz.de>
2023-10-30 12:59:26 +0100

    added Ex 1

---
+++ 01/solution.md
@@ -0,0 +1,6 @@
+# Solution Exercise 1
+Group: *Name of Group Member 1, Name of Group Member 2*
+## Task 1
+*Your solution here!*
+## Task 2
+Please edit the HTML/JS files in the ``src`` folder!
---
+++ 01/src/01.js
@@ -0,0 +1,62 @@
+"use strict";
+
+let canvas;
+let gl;
+
+let positions = [-0.5, -0.5, 0,
+                  0.5, -0.5, 0,
+                  0.0, 0.5, 0];
+let colors = [];
+
+
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2');
+    if (!gl) alert("WebGL 2.0 isn't available");
+
+  
+    //
+    //  Configure WebGL
+    //
+
+    //Specify the size of the viewport
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    //Specify the background color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+
+
+    //Load and compile shaders
+    let program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+   
+
+    //Create buffer for vertices
+    let positionBuffer = gl.createBuffer();
+
+    //bind a given WebGLBuffer to a target
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    //Pass the vertices to the buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    //Get location of attribute in shader program
+    let positionLoc = gl.getAttribLocation(program, "in_position");
+
+    //Specify the number of components in a vertex attribute
+    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    render();
+};
+
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, positions.length);
+    requestAnimationFrame(render);
+}
