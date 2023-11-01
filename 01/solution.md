819e8f44146099b440fce0094808040989c5abd2
nidanur <nidanur@coinweb.io>
2023-11-01 16:26:30 +0100

    feat(01.md): solution

--- 01/solution.md
+++ 01/solution.md
@@ -1,6 +1,11 @@
 # Solution Exercise 1
-Group: *Name of Group Member 1, Name of Group Member 2*
+Group: *Nidanur Günay, Zeyuan Yu*
 ## Task 1
-*Your solution here!*
+
+1. Games: Computer graphics are used in both video and AR/VR games
+2. Data Visualization: Computer graphics are used to visualize tthe data.
+3. Architecture: Computer graphics are used in architecture to achieve a realistic simulation of the project.
+4. Movies: Computer Graphics are used in movies to have animated screens in order to increase reality.
+5. User Interfaces: Computer Graphics are used in UI to increase the usability and user experience of the software.
 ## Task 2
 Please edit the HTML/JS files in the ``src`` folder!
--- 01/src/01.js
+++ 01/src/01.js
@@ -3,73 +3,58 @@
 let canvas;
 let gl;
 
-let positions = [-0.5, -0.5, 0,
-                  0.5, -0.5, 0,
-                  0.0, 0.5, 0];
-let colors = [1.0, 0.0, 0.0,
-              0.0, 1.0, 0.0,
-              0.0, 0.0, 1.0];
-
-
+let positions = [-0.5, -0.5, 0, 0.5, -0.5, 0, 0.0, 0.5, 0];
+let colors = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0];
 
 window.onload = function init() {
-    canvas = document.getElementById("gl-canvas");
+  canvas = document.getElementById("gl-canvas");
 
-    gl = canvas.getContext('webgl2');
-    if (!gl) alert("WebGL 2.0 isn't available");
+  gl = canvas.getContext("webgl2");
+  if (!gl) alert("WebGL 2.0 isn't available");
 
-  
-    //
-    //  Configure WebGL
-    //
+  //
+  //  Configure WebGL
+  //
 
-    //Specify the size of the viewport
-    gl.viewport(0, 0, canvas.width, canvas.height);
+  //Specify the size of the viewport
+  gl.viewport(0, 0, canvas.width, canvas.height);
 
-    //Specify the background color
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  //Specify the background color
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
+  //Load and compile shaders
+  let program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
 
+  //Create buffer for vertices
+  let positionBuffer = gl.createBuffer();
 
-    //Load and compile shaders
-    let program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+  //bind a given WebGLBuffer to a target
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-   
+  //Pass the vertices to the buffer
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-    //Create buffer for vertices
-    let positionBuffer = gl.createBuffer();
+  //Get location of attribute in shader program
+  let positionLoc = gl.getAttribLocation(program, "in_position");
 
-    //bind a given WebGLBuffer to a target
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    
-    //Pass the vertices to the buffer
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  //Specify the number of components in a vertex attribute
+  gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionLoc);
 
-    //Get location of attribute in shader program
-    let positionLoc = gl.getAttribLocation(program, "in_position");
+  //color codes, basically the same as vertex code above
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  let in_color = gl.getAttribLocation(program, "in_color");
+  gl.vertexAttribPointer(in_color, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(in_color);
 
-    //Specify the number of components in a vertex attribute
-    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
-
-    //color codes, basically the same as vertex code above
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    let in_color = gl.getAttribLocation(program, "in_color");
-    gl.vertexAttribPointer(in_color, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(in_color);
-
-
-
-
-    render();
+  render();
 };
 
-
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, positions.length/3);
-    requestAnimationFrame(render);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, positions.length / 3);
+  requestAnimationFrame(render);
 }
