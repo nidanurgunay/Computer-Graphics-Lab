16357c54aa969a175652f38dfd566423ef59c3f5
泽源 于 <1482701699@qq.com>
2023-11-26 18:34:42 +0100

    added into submission.md

--- 04/04_submission.md
+++ 04/04_submission.md
@@ -1,7 +1,35 @@
 # Solution Exercise 4
-Group: *Name of Group Member 1, Name of Group Member 2*
+Group: Nidanur Günay, Yu Zeyuan
 ## Task 1
-*Your solution here!*
+
+1. Establish a recurrence equation for $`A_n`$. (2 point)
+
+    $A_0$ = A
+
+    $A_n$ = $ (\frac{3}{4}) \cdot A_{n-1}$
+
+2. Show by complete induction that $`A_n = (\frac{3}{4})^n \cdot A`$ holds. (2 point)
+
+    assume $A_n = (\frac{3}{4})^n \cdot A$
+
+    prove $A_{n+1} = (\frac{3}{4})^{n+1} \cdot A$
+
+    from recursive equation
+
+    $A_{n+1}$ = $ (\frac{3}{4}) \cdot A_{n}$ and $A_n = (\frac{3}{4})^n \cdot A$
+
+    then
+
+     $A_{n+1}$ = $ (\frac{3}{4}) \cdot A_{n}$ =  $ (\frac{3}{4}) \cdot (\frac{3}{4})^n\cdot A$
+
+     = $A_{n+1} = (\frac{3}{4})^{n+1} \cdot A$
+
+
+
+3. What happens in the limit for $`n \rightarrow \infty`$? Determine $` \lim \limits_{n \to \infty} A_n`$. (1 points)
+
+    limit is 0 because the denominator ($4^n$) gets larger and larger when n $\to \infty$
+
 ## Task 2
 Please edit the HTML/JS files in the ``src`` folder!
 
--- 04/src/04.js
+++ 04/src/04.js
@@ -1,151 +1,147 @@
-// loading canvas element
-let canvas = document.getElementById('gl-canvas');
-canvas.width = 600;
-canvas.height = 600;
-gl = canvas.getContext("webgl2");
-if (!gl) alert("WebGL 2.0 isn't available");
-gl.enable(gl.DEPTH_TEST);
+var canvas;
+var gl;
+
+// var positions;
+
 var numTimesToSubdivide = 0;
+var bufferId;
+var points = [];
+var colors = [];
 
-let baseColors = [
-  [1.0, 0.0, 0],
-  [0, 1.0, 0],
-  [0, 0, 1.0],
-  [1.0, 0.0, 1.0]
-]
+var baseColors = [
+  vec3(1.0, 0.0, 0.0),
+  vec3(0.0, 1.0, 0.0),
+  vec3(0.0, 0.0, 1.0),
+  vec3(0.0, 0.0, 0.0),
+];
 
-// creating a simple series of points
-let vertices = [
-   [0.0000,  -0.1500, -1.0000],
-   [0.0000,  0.8428,  0.3333],
-   [-0.8165, -0.5714,  0.3333],
-   [0.8165, -0.5714,  0.3333]
-]
-let points = []
-let colors = []
+function init() {
+  canvas = document.getElementById("gl-canvas");
 
+  gl = canvas.getContext("webgl2");
+  if (!gl) alert("WebGL 2.0 isn't available");
 
-// utility to scale a vector
-function scale(point, scale) {
-  return [point[0]*scale, point[1]*scale, point[2]*scale]
+  //
+  //  Initialize our data for the Sierpinski Gasket
+  //
+
+  // First, initialize the corners of our gasket with three positions.
+
+  //
+  //  Configure WebGL
+  //
+
+  // Load the data into the GPU
+  var vertices = [
+    vec3(0.0, 0.0, -1.0),
+    vec3(0.0, 0.9428, 0.3333),
+    vec3(-0.8165, -0.4714, 0.3333),
+    vec3(0.8165, -0.4714, 0.3333),
+  ];
+  divideTetra(vertices[0], vertices[1], vertices[2], vertices[3], 5);
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+  //  Load shaders and initialize attribute buffers
+
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+  var cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+  var aColor = gl.getAttribLocation(program, "aColor");
+  gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aColor);
+
+  // Associate out shader variables with our data buffer
+  let vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+  var positionLoc = gl.getAttribLocation(program, "aPosition");
+  gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionLoc);
+  // console.log("positionLoc, aPosition", positionLoc);
+  document.getElementById("slider").onchange = function (event) {
+    numTimesToSubdivide = parseInt(event.target.value);
+    render();
+  };
+
+  render();
+}
+function triangle(a, b, c) {
+  points.push(a, b, c);
 }
 
-
-function triangle (a, b, c, color) {
-  colors.push(scale(baseColors[color], 1))
-  points.push(a)
-  colors.push(scale(baseColors[color], 1))
-  points.push(b)
-  colors.push(baseColors[color])
-  points.push(c)
+function triangle(a, b, c, color) {
+  colors.push(baseColors[color], baseColors[color], baseColors[color]);
+  points.push(a, b, c);
 }
-
-function tetra (a, b, c, d) {
-  triangle(a, c, b, 0)
-  triangle(a, c, d, 1)
-  triangle(a, b, d, 2)
-  triangle(b, c, d, 3)
+function tetra(a, b, c, d) {
+  triangle(a, c, b, 0);
+  triangle(a, c, d, 1);
+  triangle(a, b, d, 2);
+  triangle(b, c, d, 3);
 }
-
-function getMiddlePoint(u, v){
-   return [0.5*(u[0] + v[0]), 0.5* (u[1] + v[1]), 0.5* (u[2] + v[2])]
+function getMiddlePoint(u, v) {
+  return [0.5 * (u[0] + v[0]), 0.5 * (u[1] + v[1]), 0.5 * (u[2] + v[2])];
 }
-
-
 function divideTetra(a, b, c, d, count) {
-  if(count===0){
-    tetra(a, b, c, d)
-    return
+  if (count === 0) {
+    tetra(a, b, c, d);
+    return;
   }
-  let ab = getMiddlePoint(a, b)
-  let ac = getMiddlePoint(a, c)
-  let ad = getMiddlePoint(a, d)
-  let bc = getMiddlePoint(b, c)
-  let bd = getMiddlePoint(b, d)
-  let cd = getMiddlePoint(c, d)
-  --count
-  divideTetra(a, ab, ac, ad, count)
-  divideTetra(ab, b, bc, bd, count)
-  divideTetra(ac, bc, c, cd, count)
-  divideTetra(ad, bd, cd, d, count)
+
+  else {
+    let ab = getMiddlePoint(a, b);
+    let ac = getMiddlePoint(a, c);
+    let ad = getMiddlePoint(a, d);
+    let bc = getMiddlePoint(b, c);
+    let bd = getMiddlePoint(b, d);
+    let cd = getMiddlePoint(c, d);
+
+    --count;
+
+    divideTetra(a, ab, ac, ad, count);
+    divideTetra(ab, b, bc, bd, count);
+    divideTetra(ac, bc, c, cd, count);
+    divideTetra(ad, bd, cd, d, count);
+  }
 }
 
+window.onload = init;
 
-// loading vertex and fragment shaders
-let vertex = document.getElementById('vertex-shader')
-let vertexShader = gl.createShader(gl.VERTEX_SHADER)
-gl.shaderSource(vertexShader, vertex.text)
-gl.compileShader(vertexShader)
-
-// Check if the shader compiled successfully
-if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-  console.error('Vertex shader compilation error: ' + gl.getShaderInfoLog(vertexShader));
-}
-
-let fragment = document.getElementById('fragment-shader')
-let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-gl.shaderSource(fragmentShader, fragment.text)
-gl.compileShader(fragmentShader)
-
-// initializing gl program
-let program = gl.createProgram()
-gl.attachShader(program, vertexShader)
-gl.attachShader(program, fragmentShader)
-gl.linkProgram(program)
-
-// Check if the program linked successfully
-if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-  console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program));
-}
-
-gl.clearColor(0.0, 0.0, 0.0, 0.0)
-
-gl.useProgram(program)
-console.log(numTimesToSubdivide);
-
-// ... your existing code ...
-function regenerateAndRedraw() {
-  points = [];
+function render() {
+  // var vertices = [
+  //     vec2(-1, -1),
+  //     vec2(0, 1),
+  //     vec2(1, -1)
+  // ];
+  point = [];
   colors = [];
+  console.log("colors at render", colors);
+  console.log("points at render", points);
+  var vertices = [
+    vec3(0.0, 0.0, -1.0),
+    vec3(0.0, 0.9428, 0.3333),
+    vec3(-0.8165, -0.4714, 0.3333),
+    vec3(0.8165, -0.4714, 0.3333),
+  ];
+  divideTetra(
+    vertices[0],
+    vertices[1],
+    vertices[2],
+    vertices[3],
+    numTimesToSubdivide
+  );
 
-  divideTetra(vertices[0], vertices[1], vertices[2], vertices[3], numTimesToSubdivide);
-
-  let cBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW)
-
-
-  let aColor = gl.getAttribLocation(program, 'aColor');
-  
-  gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0)
-  gl.enableVertexAttribArray(aColor)
-
-  let pointBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, pointBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW)
-
-
-  let positionLoc  = gl.getAttribLocation(program, 'aPosition')
-  gl.vertexAttribPointer(positionLoc , 3, gl.FLOAT, false, 0, 0)
-  gl.enableVertexAttribArray(positionLoc )
-
-}
-
-// Call this function whenever you change numTimesToSubdivide
-// Function to regenerate and redraw the tetrahedron
-  // webgl data
-
-
-function draw() {
-  regenerateAndRedraw();
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.drawArrays(gl.TRIANGLES, 0, points.length);
+  points = [];
+  colors = [];
 }
-
-
-draw();
-document.getElementById("slider").onchange = function (event) {
-  numTimesToSubdivide = parseInt(event.target.value);
-
-  draw();
-};
